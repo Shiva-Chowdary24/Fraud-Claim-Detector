a2734272a431452aa6878941619bd8b16a9d0e48
Shiva-Chowdary24 <126014024@sastra.ac.in>
2026-04-08 10:35:28 +0530

    Create Navbar.js

---
+++ Navbar.js
@@ -0,0 +1,19 @@
+function Navbar(){
+
+return(
+
+<div className="bg-blue-900 text-white p-4 flex justify-between">
+
+<h1 className="text-xl font-bold">
+Insurance Fraud Detection
+</h1>
+
+<p>Admin Panel</p>
+
+</div>
+
+)
+
+}
+
+export default Navbar
